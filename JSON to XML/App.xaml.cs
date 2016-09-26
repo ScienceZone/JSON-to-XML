f401b66d0f62259465a0f54cd5b9f5b096ab6c1d
Alpheratz <alpheratz93@gmail.com>
2016-09-27 02:36:55 +0300

    Added two buttons, started implementing parsing: handling objects, arrays and values separately

--- JSON to XML/App.xaml.cs
+++ JSON to XML/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.IO;
+using System.Text;
 
 namespace JSON_to_XML
 {
@@ -13,5 +15,81 @@ namespace JSON_to_XML
     /// </summary>
     public partial class App : Application
     {
+        static public StreamWriter XMLWriter
+        { get; set; }
+
+        static public StreamReader JSONReader
+        { get; set; }
+
+        static string json;
+        static StringBuilder xml;
+        static int tabCount = 0;
+
+        //Parsing the whole file
+        static public void Parse(string fileName)
+        {
+            int i = 0;
+
+            JSONReader = new StreamReader(fileName);
+            json = JSONReader.ReadToEnd();
+            JSONReader.Close();
+
+            xml = new StringBuilder();
+            xml.Clear();
+
+            while(i < json.Length)
+            {
+                json = json.TrimStart();
+            }
+        }
+
+        //Parsing stuff starting with '{'
+        //Should probably add object stack to save object names for handling closing tags
+        static int ParseObject(string str, int first, StringBuilder destination)
+        {
+            destination.Append("<");
+            int i = first + 1;
+            bool exit = false;
+            while (!exit)
+            {
+                switch(str[i])
+                {
+                    case '[':
+                        ParseArray(str, i, destination);
+                        break;
+                    case '\"':
+                    case '\'':
+                        ParseQuote(str, i, destination);
+                        break;
+                    case ':':
+                        destination.Append(">\n\t");
+                        tabCount++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return first + 1;
+        }
+
+        //Parsing stuff starting with '['
+        static int ParseArray(string str, int first, StringBuilder destination)
+        {
+            return first + 1;
+        }
+
+        //Parsing stuff starting with quotation marks, either single or double
+        static int ParseQuote(string str, int first, StringBuilder destination)
+        {
+            int t = 1;
+            do
+            {
+                t = str.IndexOf(str[first], first + 1);
+            } while (str[t - 1] != '\\');
+
+            destination.Append(str.Substring(first + 1, t - first - 1));
+            
+            return t;
+        }
     }
 }
--- JSON to XML/MainWindow.xaml.cs
+++ JSON to XML/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace JSON_to_XML
 {
@@ -20,9 +22,39 @@ namespace JSON_to_XML
     /// </summary>
     public partial class MainWindow : Window
     {
+        OpenFileDialog openJsonDlg;
+        SaveFileDialog saveXmlDlg;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            openJsonDlg = new OpenFileDialog();
+
+            openJsonDlg.DefaultExt = ".json";
+            openJsonDlg.Filter = "JSON files(*.json)|*.json";
+            openJsonDlg.CheckFileExists = true;
+            openJsonDlg.CheckPathExists = true;
+            openJsonDlg.Multiselect = false;
+
+            saveXmlDlg = new SaveFileDialog();
+
+            saveXmlDlg.DefaultExt = ".xml";
+            saveXmlDlg.Filter = "XML files(*.xml)|*.xml";
+            saveXmlDlg.AddExtension = true;
+            saveXmlDlg.CreatePrompt = true;
+            saveXmlDlg.OverwritePrompt = true;
+        }
+
+        private void openJsonButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (openJsonDlg.ShowDialog() == true)
+                App.Parse(openJsonDlg.FileName);
+        }
+
+        private void saveXmlButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
