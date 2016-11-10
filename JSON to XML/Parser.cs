a821b38d7e9a753b5e0697336467198960869d20
Alpheratz <alpheratz93@gmail.com>
2016-11-11 00:49:04 +0300

    Moved parsing into a separate class; also fixed json string trimming and colon character searching

---
+++ JSON to XML/Parser.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace JSON_to_XML
+{
+    class Parser
+    {
+        public static XmlWriterSettings XmlSettings
+        { get; set; }
+
+        public static string RootElementName { get; set; }
+        
+        static Parser()
+        {
+            XmlSettings = new XmlWriterSettings();
+            XmlSettings.Indent = true;
+            XmlSettings.IndentChars = "\t";
+
+            RootElementName = "root";
+        }
+
+        public static XmlDocument JsonToXml(string json)
+        {
+            XmlDocument xml = new XmlDocument();
+            XmlNode root = xml.CreateElement(RootElementName);
+
+            json = json.Trim();
+
+            try
+            {
+                if (json[0] == '{')
+                    ParseObject(json, root);
+                if (json[0] == '[')
+                    ParseArray(json, root);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+
+            xml.AppendChild(root);
+            return xml;
+        }
+
+        ///<summary>
+        ///Parses a JSON object.
+        ///Assuming the string starts with '{' and ends with '}'
+        ///</summary>
+        static void ParseObject(string jsonObject, XmlNode currentNode)
+        {
+            jsonObject = jsonObject.Substring(1, jsonObject.Length - 2).Trim();
+            int curlyBrackets = 0, brackets = 0, lastPairStart = 0;
+            bool areQuotesOk = true;
+
+            for (int i = 0; i < jsonObject.Length; i++)
+            {
+                switch (jsonObject[i])
+                {
+                    case ',':
+                        if (curlyBrackets == 0 && brackets == 0 && areQuotesOk)
+                        {
+                            try
+                            {
+                                ParsePair(jsonObject.Substring(lastPairStart, i - lastPairStart).Trim(), currentNode);
+                            }
+                            catch (ArgumentException e)
+                            {
+                                throw e;
+                            }
+                            lastPairStart = i + 1;
+                        }
+                        break;
+                    case '[':
+                        brackets++;
+                        break;
+                    case '{':
+                        curlyBrackets++;
+                        break;
+                    case ']':
+                        brackets--;
+                        break;
+                    case '}':
+                        curlyBrackets--;
+                        break;
+                    case '\"':
+                        areQuotesOk = !areQuotesOk;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            try
+            {
+                ParsePair(jsonObject.Substring(lastPairStart, jsonObject.Length - lastPairStart).Trim(), currentNode);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// Parses a JSON array.
+        /// </summary>
+        /// <param name="jsonArray">A string that represents a JSON array.
+        /// Assuming that it starts with '[' and ends with ']'. </param>
+        /// <param name="parent"></param>
+        /// <returns></returns>
+        //It almost duplicates the ParseObject method, gotta think how to get rid of that
+        static void ParseArray(string jsonArray, XmlNode parent)
+        {
+            int curlyBrackets = 0, brackets = 0, lastValueStart = 0;
+            bool areQuotesOk = true;
+
+            jsonArray = jsonArray.Substring(1, jsonArray.Length - 2).Trim();
+
+            for (int i = 0; i < jsonArray.Length; i++)
+            {
+                switch (jsonArray[i])
+                {
+                    case ',':
+                        if (curlyBrackets == 0 && brackets == 0 && areQuotesOk)
+                        {
+                            XmlNode node = parent.OwnerDocument.CreateElement("element");
+                            try
+                            {
+                                ParseValue(jsonArray.Substring(lastValueStart, i - lastValueStart).Trim(), node);
+                            }
+                            catch (ArgumentException e)
+                            {
+                                throw e;
+                            }
+                            lastValueStart = i + 1;
+                            parent.AppendChild(node);
+                        }
+                        break;
+                    case '[':
+                        brackets++;
+                        break;
+                    case '{':
+                        curlyBrackets++;
+                        break;
+                    case ']':
+                        brackets--;
+                        break;
+                    case '}':
+                        curlyBrackets--;
+                        break;
+                    case '\"':
+                        areQuotesOk = !areQuotesOk;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            XmlNode lastNode = parent.OwnerDocument.CreateElement("element");
+            try
+            {
+                ParseValue(jsonArray.Substring(lastValueStart, jsonArray.Length - lastValueStart).Trim(), lastNode);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            parent.AppendChild(lastNode);
+        }
+
+        ///<summary>
+        ///Parses a name:value pair within a JSON object.
+        ///</summary>
+        static void ParsePair(string jsonPair, XmlNode parentNode)
+        {
+            bool areQuotesOk = true;
+            int colonIndex = 0;
+            while (colonIndex < jsonPair.Length - 1 && !(jsonPair[colonIndex] == ':' && areQuotesOk))
+            {
+                if (jsonPair[colonIndex] == '"')
+                    areQuotesOk = !areQuotesOk;
+                colonIndex++;
+            }
+
+            if (colonIndex == jsonPair.Length - 1)
+                throw new ArgumentException(string.Format("Pair: [{0}] has no value.", jsonPair), "str");
+
+            string name = jsonPair.Substring(0, colonIndex).Trim().Trim('\"');
+            if (name.Length == 0)
+                throw new ArgumentException(string.Format("Pair [{0}] has no name.", jsonPair), "str");
+            XmlNode node = parentNode.OwnerDocument.CreateElement(name);
+            try
+            {
+                ParseValue(jsonPair.Substring(colonIndex + 1).Trim(), node);
+            }
+            catch (ArgumentException e)
+            {
+                throw e;
+            }
+            parentNode.AppendChild(node);
+        }
+
+        ///<summary>
+        ///Parses the value of a JSON name:value pair or a JSON array element.
+        ///</summary>
+        static void ParseValue(string jsonValue, XmlNode currentNode)
+        {
+            //less lines than if i used a switch block
+            if (jsonValue[0] != '{' && jsonValue[0] != '[')
+            {
+                currentNode.InnerText = jsonValue.Trim().Trim('\"');
+            }
+            else
+            {
+                try
+                {
+                    if (jsonValue[0] == '{')
+                        ParseObject(jsonValue, currentNode);
+                    else
+                        ParseArray(jsonValue, currentNode);
+                }
+                catch (ArgumentException e)
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
